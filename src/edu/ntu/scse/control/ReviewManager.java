d0d0cefcf010a5d2cd9f283ef8bd91196ffc2b56
Hither1 <42910536+Hither1@users.noreply.github.com>
2019-10-28 23:13:56 +0800

    Add files via upload

---
+++ src/edu/ntu/scse/control/ReviewManager.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * 
+ * @author suhuangyuan
+ *
+ */
+public class ReviewManager {
+
+	private ArrayList<Review> reviews;
+	
+	/**
+	 * Create a new reivew and add it to the list
+	 * @param movie
+	 * @param movieGoer
+	 * @param reviewText
+	 * @param rating
+	 * @return
+	 */
+	public Review createReview(Movie movie, MovieGoer movieGoer,String reviewText,int rating) {
+		// Simply increment the id by 1 each time a new ID is added.
+		int id = reviews.size()+1;
+		
+		Review newReview = new Review(id,reviewText,rating,movieGoer);
+		//Add new review to the list
+		reviews.add(newReview);
+		return newReview;
+	}
+}
---
+++ src/edu/ntu/scse/control/StaffManager.java
@@ -0,0 +1,96 @@
+//package edu.ntu.scse.entity;
+import java.util.ArrayList;
+/**
+ * 
+ * @author suhuangyuan
+ *
+ */
+public class StaffManager {
+
+	/**
+	 * Login a staff
+	 * @param inputId
+	 * @param inputPassword
+	 * @param listStaffs
+	 * @return
+	 */
+	public boolean login(int inputId,String inputPassword,ArrayList<Staff> listStaffs) {
+		for(Staff staff:listStaffs) {
+			if(staff.getCinemaStaffId() == inputId) {
+				if(staff.getPassword() == inputPassword) {
+					// If both ID and password match
+					System.out.println("Welcome!");
+					return true;
+				}
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Add a new movie to the list
+	 * @param listMovies
+	 * @param movieId
+	 * @param title
+	 * @param synopsis
+	 * @param director
+	 * @param cast
+	 * @param showingStatus
+	 * @param movieType
+	 * @param isBlockbuster
+	 * @return
+	 */
+	public ArrayList<Movie> addMovieToList(ArrayList<Movie> listMovies, int movieId,String title,String synopsis,String director,String cast,MovieStatus mvStatus,MovieType movieType,boolean isBlockbuster,MovieRating movieRating){
+		//When a movie is first added, it has no review
+		float overRating = 0;
+		Movie newMovie = new Movie(movieId,title,synopsis,director,cast,isBlockbuster,overRating,movieRating,mvStatus,movieType);
+		listMovies.add(newMovie);
+		return listMovies;
+	}
+	
+	/**
+	 * Update some attribute of an existent movie that is already in the list
+	 * @param listMovies
+	 * @param movieId
+	 * @param title
+	 * @param synopsis
+	 * @param director
+	 * @param cast
+	 * @param showingStatus
+	 * @param movieType
+	 * @param isBlockbuster
+	 * @return
+	 */
+	public ArrayList<Movie> UpdateMovieListing(ArrayList<Movie> listMovies,int movieId, String title, String synopsis,String director,String cast,MovieStatus movieStatus,MovieType movieType,boolean isBlockbuster,MovieRating movieRating){
+		for(Movie movie:listMovies) {
+			if(movie.getMovieId() == movieId) {
+				movie.setBlockbuster(isBlockbuster);
+				movie.setTitle(title);
+				movie.setSynopsis(synopsis);
+				movie.setDirector(director);
+				movie.setCast(cast);
+				movie.setMovieStatus(movieStatus);
+				movie.setMovieType(movieType);
+			}
+		}
+		return listMovies;
+	}
+	
+	/**
+	 * Remove a movie by Id
+	 * @param listMovies
+	 * @param movieId
+	 * @return
+	 */
+	public ArrayList<Movie> removeMovieFromList(ArrayList<Movie> listMovies,int movieId){
+		for(Movie movie:listMovies) {
+			if(movie.getMovieId() == movieId) {
+				listMovies.remove(movie);
+			}
+		}
+		return listMovies;
+	}
+	
+	
+}
