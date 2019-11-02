b0891242ec3698e99f42044855685a8290731a15
kailing231 <kailing231@ymail.com>
2019-11-02 18:41:21 +0800

    added package path and auto format code

--- src/edu/ntu/scse/entity/Review.java
+++ src/edu/ntu/scse/entity/Review.java
@@ -1,3 +1,5 @@
+package edu.ntu.scse.entity;
+
 /**
  * 
  * @author suhuangyuan
@@ -14,16 +16,17 @@ public class Review {
 	 */
 	private String reviewText;
 	/**
-	 * rating 
+	 * rating
 	 */
 	private int rating;
 	/**
 	 * movieGoer(author of the movie)
 	 */
 	private MovieGoer movieGoer;
-	
+
 	/**
 	 * Constructor of a movie
+	 * 
 	 * @param reviewId
 	 * @param reviewText
 	 * @param rating
@@ -36,45 +39,53 @@ public class Review {
 		this.rating = rating;
 		this.movieGoer = movieGoer;
 	}
+
 	public int getReviewId() {
 		return reviewId;
 	}
+
 	public void setReviewId(int reviewId) {
 		this.reviewId = reviewId;
 	}
+
 	public String getReviewText() {
 		return reviewText;
 	}
+
 	public void setReviewText(String reviewText) {
 		this.reviewText = reviewText;
 	}
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
 	public MovieGoer getMovieGoer() {
 		return movieGoer;
 	}
+
 	public void setMovieGoer(MovieGoer movieGoer) {
 		this.movieGoer = movieGoer;
 	}
-	
+
 	@Override
-    public boolean equals(Object obj) {
-        if (!obj.getClass().equals(this.getClass())) {
-            return false;
-        }
+	public boolean equals(Object obj) {
+		if (!obj.getClass().equals(this.getClass())) {
+			return false;
+		}
 
-        if (this.getReviewId()==((Review)obj).getReviewId() &&
-                this.getReviewText().equals(((Review)obj).getReviewText()) &&
-                this.getRating() == ((Review)obj).getRating() &&
-                this.getMovieGoer().equals(((Review)obj).getMovieGoer())) {
-            return true;
-        }
+		if (this.getReviewId() == ((Review) obj).getReviewId()
+				&& this.getReviewText().equals(((Review) obj).getReviewText())
+				&& this.getRating() == ((Review) obj).getRating()
+				&& this.getMovieGoer().equals(((Review) obj).getMovieGoer())) {
+			return true;
+		}
 
-        return false;
-    }
-	
+		return false;
+	}
+
 }
--- src/edu/ntu/scse/entity/Staff.java
+++ src/edu/ntu/scse/entity/Staff.java
@@ -1,9 +1,11 @@
+package edu.ntu.scse.entity;
+
 /**
  * 
  * @author suhuangyuan
  *
  */
-public class Staff extends Person{
+public class Staff extends Person {
 	/**
 	 * ID of staff
 	 */
@@ -14,7 +16,9 @@ public class Staff extends Person{
 	private String password;
 
 	/**
-	 * Constructor for Staff with all attributes and default constructor from super class
+	 * Constructor for Staff with all attributes and default constructor from super
+	 * class
+	 * 
 	 * @param cinemaStaffId
 	 * @param password
 	 */
@@ -23,38 +27,42 @@ public class Staff extends Person{
 		this.cinemaStaffId = cinemaStaffId;
 		this.password = password;
 	}
+
 	public int getCinemaStaffId() {
 		return cinemaStaffId;
 	}
+
 	public void setCinemaStaffId(int cinemaStaffId) {
 		this.cinemaStaffId = cinemaStaffId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 //	public Staff() {
 //		super();
 //	}
 //	
 	@Override
-    public boolean equals(Object obj) {
-        if (!obj.getClass().equals(this.getClass())) {
-            return false;
-        }
+	public boolean equals(Object obj) {
+		if (!obj.getClass().equals(this.getClass())) {
+			return false;
+		}
 
-        if (this.getLastName().equals(((Person)obj).getLastName()) &&
-                this.getFirstName().equals(((Person)obj).getFirstName()) &&
-                this.getEmail().equals(((Person)obj).getEmail())&&
-                this.getPhoneNo().equals(((Person)obj).getPhoneNo())&&
-        		this.getCinemaStaffId()==((Staff)obj).getCinemaStaffId() &&
-                this.getPassword().equals(((Staff)obj).getPassword())) {
-            return true;
-        }
+		if (this.getLastName().equals(((Person) obj).getLastName())
+				&& this.getFirstName().equals(((Person) obj).getFirstName())
+				&& this.getEmail().equals(((Person) obj).getEmail())
+				&& this.getPhoneNo().equals(((Person) obj).getPhoneNo())
+				&& this.getCinemaStaffId() == ((Staff) obj).getCinemaStaffId()
+				&& this.getPassword().equals(((Staff) obj).getPassword())) {
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
