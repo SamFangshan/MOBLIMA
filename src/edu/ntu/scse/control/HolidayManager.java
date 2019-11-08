8730b3f1da5be91a5464aa9ed72d9c113a255885
Zilvinas <zilvinas.skuodys@gmail.com>
2019-11-08 19:02:57 +0800

    holiday manager

--- src/edu/ntu/scse/control/HolidayManager.java
+++ src/edu/ntu/scse/control/HolidayManager.java
@@ -5,20 +5,48 @@ import edu.ntu.scse.entity.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
+import java.io.*;
 
 /**
- *
+ * HolidayManager
+ * @author Zilvinas
  */
+
 public class HolidayManager {
 
-    private ArrayList<Holiday> holidays;
     private String filename;
 
     public HolidayManager(String filename) {
         this.filename = filename;
     }
 
-    private ArrayList<Holiday> readHolidays() {
+    /**
+     * update find and replace current entry of a holiday
+     * @param holidays
+     * @param holiday entry to be updated
+     */
+    public void updateHolidays(ArraList<Holiday> holidays, Holiday holiday) {
+        boolean found = false;
+        for(int i = 0; i < holidays.size(); i++) {
+            if(holiday.getHolidayId() == holidays.get(i).getHolidayId()) {
+                holidays.remove(holidays.get(i));   //remove old entry of the holiday
+                holidays.add(holiday);              //add new entry
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("No holiday was found to be updated.");
+        }
+        else {
+            System.out.println("Data for holiday " + holiday.getName() + " has been updated");
+        }
+    }
+
+    /**
+     * read holidays from file
+     * @return holidays
+     */
+    public ArrayList<Holiday> readHolidays() {
         ArrayList<Holiday> holidays = new ArrayList<Holiday>();
 
         try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
@@ -27,7 +55,8 @@ public class HolidayManager {
                 String[] tokens = line.split(" |\\|");
 
                 if(tokens[0].equals("Holiday")) { //unsure about calendar date construction
-                    holidays.add(new Holiday(Integer.parseInt(tokens[2]), tokens[5], new Calendar(Integer.parseInt(tokens[8]))));
+                    holidays.add(new Holiday(Integer.parseInt(tokens[2]), tokens[5],
+                            Calendar.getInstance().set(Calendar.DATE, Integer.parseInt(tokens[8]));
                 }
                 else {
                     System.out.println("Error reading data.");
@@ -44,26 +73,29 @@ public class HolidayManager {
     }
 
     /**
-     * add new holiday
+     * add new holiday to the list
+     * @param holidays
      * @param holiday
      */
-    public void addNewHoliday(Holiday holiday) {
+    public void addNewHoliday(ArraList<Holiday> holidays, Holiday holiday) {
         holidays.add(holiday);
     }
 
     /**
      * remove holiday object
+     * @param holidays
      * @param holiday
      */
-    public void removeHoliday(Holiday holiday) {
+    public void removeHoliday(ArraList<Holiday> holidays, Holiday holiday) {
         holidays.remove(holiday);
     }
 
     /**
      * remove holiday by holidayId
+     * @param holidays
      * @param holidayId
      */
-    public void removeHoliday(int holidayId) {
+    public void removeHoliday(ArraList<Holiday> holidays, int holidayId) {
         for(int i = 0; i < holidays.size(); i++) {
             if(holidays.get(i).getHolidayId() == holidayId) {
                 holidays.remove(holidays.get(i));
@@ -72,14 +104,35 @@ public class HolidayManager {
     }
 
     /**
-     * remove holiday by name
+     * remove holiday by holiday name
+     * @param holidays
      * @param holidayName
      */
-    public void removeHoliday(String holidayName) {
+    public void removeHoliday(ArraList<Holiday> holidays, String holidayName) {
         for(int i = 0; i < holidays.size(); i++) {
             if(holidays.get(i).getName() == holidayId) {
                 holidays.remove(holidays.get(i));
             }
+        }
+    }
+
+    /**
+     * write holidays to file
+     * @param holidays
+     */
+    public void writeHolidaysToFile(ArrayList<Holiday> holidays) {
+        try {
+            PrintWriter out = new PrintWriter(filename);
+
+            // save all Movie
+            for (int i = 0; i < holidays.size(); i++) {
+                String line = holidays.get(i).toString(); // generate line
+                out.println(line); // add a line to text file
+            }
+
+            out.close(); // close before exit
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
