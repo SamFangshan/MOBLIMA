02c3afb5a352dce0e44d5272a4b0f9d286bd795c
Hither1 <42910536+Hither1@users.noreply.github.com>
2019-11-11 15:48:40 +0800

    Add files via upload

--- src/edu/ntu/scse/entity/MovieGoer.java
+++ src/edu/ntu/scse/entity/MovieGoer.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * @author suhuangyuan
  *
  */
-public class MovieGoer extends Person{
+public class MovieGoer extends Person {
 
 	/**
 	 * ID of movie goer
@@ -25,14 +25,16 @@ public class MovieGoer extends Person{
 	 * reviews history of movieGoer
 	 */
 	private ArrayList<Review> reviews;
+
 	/**
 	 * Constructor of MovieGoer with all attributes and default constructor from super class
+	 *
 	 * @param movieGoerId
 	 * @param age
 	 * @param bookings
 	 * @param reviews
 	 */
-	public MovieGoer(String email,int movieGoerId, int age, ArrayList<Booking> bookings, ArrayList<Review> reviews) {
+	public MovieGoer(String email, int movieGoerId, int age, ArrayList<Booking> bookings, ArrayList<Review> reviews) {
 		super();
 		this.setEmail(email);
 		this.movieGoerId = movieGoerId;
@@ -40,27 +42,35 @@ public class MovieGoer extends Person{
 		this.bookings = bookings;
 		this.reviews = reviews;
 	}
+
 	public int getMovieGoerId() {
 		return movieGoerId;
 	}
+
 	public void setMovieGoerId(int movieGoerId) {
 		this.movieGoerId = movieGoerId;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public ArrayList<Booking> getBookings() {
 		return bookings;
 	}
+
 	public void setBookings(ArrayList<Booking> bookings) {
 		this.bookings = bookings;
 	}
+
 	public ArrayList<Review> getReviews() {
 		return reviews;
 	}
+
 	public void setReviews(ArrayList<Review> reviews) {
 		this.reviews = reviews;
 	}
@@ -75,5 +85,33 @@ public class MovieGoer extends Person{
 				getAge() == movieGoer.getAge() &&
 				getBookings().equals(movieGoer.getBookings()) &&
 				getReviews().equals(movieGoer.getReviews());
+	}
+
+	@Override
+	public String toString() {
+		return "MovieGoer|" + email + "|" + movieGoerId + "|" + age + "|" + bookingsToString(bookings) + "|" + reviewsToString(reviews);
+	}
+
+	private String bookingsToString(ArrayList<Booking> bookings){
+		String r = "";
+		if(bookings.size() != 0){
+			r += bookings.get(0).getTID();
+			for(int i=1; i<bookings.size(); i++){
+				r += ", ";
+				r += bookings.get(i).getTID();
+			}
+		}
+		return r;
+	}
+	private String reviewsToString(ArrayList<Review> reviews){
+		String r = "";
+		if(reviews.size() != 0){
+			r += reviews.get(0).getReviewId();
+			for(int i=1;i<reviews.size();i++){
+				r += ", ";
+				r += reviews.get(i).getReviewId();
+			}
+		}
+		return r;
 	}
 }
